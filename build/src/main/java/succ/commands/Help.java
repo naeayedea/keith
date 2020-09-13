0011fb95cf185f297e1e4bd4b646c49aab662341
naeayedea <steven.muirhead1999@gmail.com>
2020-09-14 00:34:32 +0100

    provided functionality for help command to print available commands and their description

--- build/src/main/java/succ/commands/Help.java
+++ build/src/main/java/succ/commands/Help.java
@@ -1,14 +1,25 @@
 package succ.commands;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import succ.commands.Command;
+import succ.commands.admin.AdminCommand;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Prints a list of all user commands and their descriptions
  */
-public class Help implements Command{
+public class Help implements Command {
+
+    private Map<String, Command> commands;
+    public Help(Map<String, Command> commands){
+        this.commands = commands;
+    }
+
     @Override
     public String getDescription() {
-        return null;
+        return "help: \"this command, returns general command list\"";
     }
 
     @Override
@@ -18,6 +29,12 @@ public class Help implements Command{
 
     @Override
     public void run(MessageReceivedEvent event) {
+        String helpString = "```cs\n";
+        for(Map.Entry<String, Command> set : commands.entrySet()){
+            helpString+= set.getValue().getDescription()+"\n";
+        }
+        helpString+="```";
+        event.getChannel().sendMessage(helpString).queue();
 
     }
 }
