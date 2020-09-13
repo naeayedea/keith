0ab0b3c6b78311cce325bf275c36577b989b8343
naeayedea <steven.muirhead1999@gmail.com>
2020-09-14 00:33:17 +0100

    refactored generic commands into their own package, UserCommand implements Command interface

--- build/src/main/java/succ/commands/generic/UserCommand.java
+++ build/src/main/java/succ/commands/generic/UserCommand.java
@@ -1,7 +1,9 @@
-package succ.commands;
+package succ.commands.generic;
+
+import succ.commands.Command;
 
 //Commands which everyone except banned users can access
-public abstract class UserCommand implements Command{
+public abstract class UserCommand implements Command {
 
     public int getAccessLevel(){
         return 1;
