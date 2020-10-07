e30a14c939a2db1be4318da2b71fe76e17261975
naeayedea <steven.muirhead1999@gmail.com>
2020-10-07 22:53:26 +0100

    added guild join and guild leave event handling

--- build/src/main/java/succ/EventHandler.java
+++ build/src/main/java/succ/EventHandler.java
@@ -1,18 +1,27 @@
 package succ;
 
-import net.dv8tion.jda.api.entities.PrivateChannel;
-import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import succ.commands.admin.Admin;
 import succ.commands.Command;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import succ.commands.admin.SetStatus;
 import succ.commands.generic.*;
 import succ.util.Database;
+import succ.util.Server;
 import succ.util.ServerManager;
 import succ.util.UserManager;
 import succ.util.logs.ConsoleLogger;
+
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.*;
@@ -20,7 +29,7 @@ import java.util.concurrent.*;
 /**
  * Main message handler for keithv2, receives incoming messages and performs tasks depending on message content
  */
-public class MessageHandler extends ListenerAdapter {
+public class EventHandler extends ListenerAdapter {
 
     JDA jda;
     Map<String, Command> commands;
@@ -29,7 +38,7 @@ public class MessageHandler extends ListenerAdapter {
     UserManager userManager;
     ServerManager serverManager;
     ExecutorService commandExecutor = Executors.newCachedThreadPool();
-    public MessageHandler(JDA jda, String url){
+    public EventHandler(JDA jda, String url){
         this.jda = jda;
         database = new Database(url);
         log = new ConsoleLogger();
@@ -37,6 +46,7 @@ public class MessageHandler extends ListenerAdapter {
         serverManager = new ServerManager(database);
         initialiseCommands();
         commandExecutor = Executors.newCachedThreadPool();
+        jda.getPresence().setActivity(Activity.playing("?help for commands | "+jda.getGuilds().size()+ " servers"));  //Default discord status
     }
 
     //Populate hashmap with all available commands, when key is entered the relevant command is returned which can be ran.
@@ -120,7 +130,29 @@ public class MessageHandler extends ListenerAdapter {
         }).start();
     }
 
-    //Performs tasks relevant to public messages
+    @Override
+    public void onGuildJoin(GuildJoinEvent event){
+        //Attempt to retrieve server from database, will create new entry if not there
+        Guild guild = event.getGuild();
+        Server server = serverManager.getServer(event.getGuild().getId());
+        TextChannel defaultChannel = guild.getDefaultChannel();
+        defaultChannel.sendMessage(new EmbedBuilder()
+                .setColor(new Color(155,0,155))
+                .setTitle("Hello!")
+                .setFooter("Use "+server.getPrefix()+"feedback if you have any issues!- Succ")
+                .setDescription("Use "+server.getPrefix()+"help to see available commands")
+                .setThumbnail(jda.getSelfUser().getAvatarUrl())
+                .build()).queue();
+        log.printSuccess("New Server "+guild+" has added the bot!");
+        new SetStatus(jda, serverManager).update();
+    }
+
+    @Override
+    public void onGuildLeave(GuildLeaveEvent event){
+        log.printWarning("Server "+event.getGuild()+" has kicked the bot :(");
+        new SetStatus(jda, serverManager).update();
+    }
+                             //Performs tasks relevant to public messages
     private void publicMessageReceived(MessageReceivedEvent event){
         log.printPublicMessage(event.getAuthor().getName()+": "+event.getMessage().getContentDisplay());
     }
