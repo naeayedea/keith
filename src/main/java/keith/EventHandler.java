9c7343c5ada4ee3016895b9b9a233f71649f3ccd
naeayedea <steven.muirhead1999@gmail.com>
2021-06-20 22:48:19 +0100

    implemented server loading/creation and added basic checks to eventhandler

--- src/main/java/keith/EventHandler.java
+++ src/main/java/keith/EventHandler.java
@@ -1,9 +1,12 @@
 package keith;
 
+import keith.managers.ServerManager;
+import keith.managers.UserManager;
 import keith.util.Database;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.ReconnectedEvent;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -16,6 +19,8 @@ import java.util.concurrent.Executors;
 public class EventHandler extends ListenerAdapter {
 
     ExecutorService commandService;
+    ServerManager serverManager;
+    UserManager userManager;
 
     public EventHandler(DataSource database, JDA jda) {
         initialise(database, jda);
@@ -25,6 +30,8 @@ public class EventHandler extends ListenerAdapter {
         initialiseCommands();
         jda.getPresence().setActivity(Activity.playing("?help for commands | "+jda.getGuilds().size()+ " servers"));
         commandService = Executors.newCachedThreadPool();
+        serverManager = ServerManager.getInstance();
+        userManager = UserManager.getInstance();
         Database.setSource(database);
     }
 
@@ -35,10 +42,21 @@ public class EventHandler extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         new Thread(() -> {
+            ServerManager.Server server = serverManager.getServer(event.getGuild().getId());
+            UserManager.User user = userManager.getUser(event.getAuthor().getId());
+            Message message = event.getMessage();
             //if user or server not banned
+            if(!user.isBanned() && !server.isBanned() && !event.getAuthor().isBot()) {
                 //check for prefix
+                if(message.getContentRaw().substring(server.getPrefix().length()).equals(server.getPrefix())) {
                     //check for valid command
                         //execute
+                    //else check for channel command
+                        //process
+                    //else ignore
+                }
+            }
+
         }).start();
     }
 
--- src/main/java/keith/managers/ServerManager.java
+++ src/main/java/keith/managers/ServerManager.java
@@ -1,13 +1,73 @@
 package keith.managers;
 
+import keith.commands.AccessLevel;
 import keith.util.Database;
 import net.dv8tion.jda.api.entities.Guild;
 
 import java.sql.PreparedStatement;
+import java.time.Instant;
+import java.util.ArrayList;
 
 public class ServerManager {
 
     private static ServerManager instance;
+
+    public static class Server {
+
+
+        private final String serverID;
+        private final String firstSeen;
+        private String prefix;
+        private Boolean banned;
+        private String pin_channel;
+
+        public Server(String serverID, String firstSeen, String prefix, Boolean banned, String pin_channel) {
+
+            this.serverID = serverID;
+            this.firstSeen = firstSeen;
+            this.prefix = prefix;
+            this.banned = banned;
+            this.pin_channel = pin_channel;
+        }
+
+
+        public String getPin_channel() {
+            return pin_channel;
+        }
+
+        public void setPin_channel(String pin_channel) {
+            this.pin_channel = pin_channel;
+        }
+
+        public Boolean isBanned() {
+            return banned;
+        }
+
+        public void setBanned(Boolean banned) {
+            this.banned = banned;
+        }
+
+        public String getPrefix() {
+            return prefix;
+        }
+
+        public void setPrefix(String newPrefix) {
+            Database.executeUpdate(prefixStatement(), newPrefix, this.prefix);
+            prefix = newPrefix;
+        }
+
+        private PreparedStatement prefixStatement() {
+            return Database.prepareStatement("UPDATE servers SET prefix = ? WHERE ServerID = ?");
+        }
+
+        public String getFirstSeen() {
+            return firstSeen;
+        }
+
+        public String getServerID() {
+            return serverID;
+        }
+    }
 
     public static ServerManager getInstance() {
         if (instance == null) {
@@ -16,23 +76,25 @@ public class ServerManager {
         return instance;
     }
 
-    private PreparedStatement createServerStatement() {
+    private PreparedStatement addServer() {
         return Database.prepareStatement("INSERT INTO servers (ServerID) VALUES (?)");
     }
 
-    private PreparedStatement prefixStatement() {
-        return Database.prepareStatement("SELECT prefix FROM servers WHERE ServerID = ?");
+    //String serverID, String firstSeen, String prefix, Boolean banned, String pin_channel
+    private PreparedStatement getServer() {
+        return Database.prepareStatement("SELECT FirstSeen, prefix, banned, pin_channel FROM servers where serverID = ?");
     }
 
-    public void addServer(Guild guild) {
-        Database.executeUpdate(createServerStatement(), guild.getId());
+    public Server getServer(String guildID) {
+        ArrayList<String> results = Database.getStringResult(getServer(), guildID);
+        if(results.size() > 1) {
+            String[] result = results.get(1).split("\\s+");
+            return new Server(guildID, result[0], result[1], Boolean.parseBoolean(result[2]), result[3]);
+        } else {
+            //server doesn't exist yet, create
+            Database.executeUpdate(addServer(), guildID);
+            return new Server(guildID, Instant.now().toString(),"?", false, null);
+        }
     }
 
-    public String getPrefix(Guild guild) {
-        return Database.getStringResult(prefixStatement(), guild.getId()).get(0);
-    }
-
-    public void setPrefix(Guild guild, String newPrefix) {
-
-    }
 }
